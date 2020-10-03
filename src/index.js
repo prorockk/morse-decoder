94aaefff102dd6674e883aa4c80e05a5252e2bc8
Капралов Яков Владиславович <70843236+prorockk@users.noreply.github.com>
2020-10-04 02:53:08 +0400

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr=[];
+    let result='';
+    let moorse=[];
+    let moorseStr='';
+    let str='';
+    for (i=0; i<expr.length;i+=10){
+        arr.push(expr.slice(i,i+10))    
+    }
+    for (i=0; i<arr.length;i++){
+        str = arr[i]+'';
+        moorseStr='';
+        if (str=='**********'){
+            moorse.push(' ')
+        }
+        else{
+            for (j=0;j<str.length;j+=2){                                   
+                    if (str.slice(j,j+2)=='10'){
+                        moorseStr+='.'
+                    }
+                     else if (str.slice(j,j+2)=='11') {
+                        moorseStr+='-'
+                    }    
+            }      
+        }        
+        if (moorseStr!=''){moorse.push(moorseStr)}    
+    }
+    for (i=0; i<moorse.length;i++){
+        
+        if (moorse[i]==' '){
+            result+=' '
+        }
+        else {result+=MORSE_TABLE[moorse[i]]}
+    }
+    return result;
 }
 
 module.exports = {
